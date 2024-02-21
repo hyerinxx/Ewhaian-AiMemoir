d7e9acf96e265e0288a83ba1e1d1700cb9b52a0d
Hyerin Sung <hyerinx@ewhain.net>
2024-02-21 18:36:58 +0900

    feat: Add font styled-components
    - 글꼴과 크기를 지정해놓은 styled-components 추가

---
+++ src/styles/Text.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const H1 = styled.div`
+    font-family: "Noto Serif KR";
+    font-size: 60px;
+`;
+
+export const H2 = styled.div`
+    font-family: "Noto Serif KR";
+    font-size: 48px;
+`;
+
+export const H3 = styled.div`
+    font-family: "Noto Serif KR";
+    font-size: 36px;
+`;
+
+export const H4 = styled.div`
+    font-family: "Noto Serif KR";
+    font-size: 24px;
+`;
