56811ca7d1decb5e1a8589c862c27b85176adddd
Hyerin Sung <hyerinx@ewhain.net>
2024-02-24 12:43:59 +0900

    Refactor: Edit Text.jsx for better readability
    - 폰트 사이즈가 중요한 컴포넌트이므로 폰트 사이즈를 첫줄에 작성
    - 구조성과 통일성을 갖추기 위해 div 대신 h 태그를 활용하여 styled-components 생성

--- src/styles/Text.jsx
+++ src/styles/Text.jsx
@@ -1,21 +1,21 @@
 import styled from 'styled-components';
 
-export const H1 = styled.div`
-    font-family: "Noto Serif KR";
+export const H1 = styled.h1`
     font-size: 60px;
+    font-family: "Noto Serif KR";
 `;
 
-export const H2 = styled.div`
-    font-family: "Noto Serif KR";
+export const H2 = styled.h2`
     font-size: 48px;
+    font-family: "Noto Serif KR";
 `;
 
-export const H3 = styled.div`
-    font-family: "Noto Serif KR";
+export const H3 = styled.h3`
     font-size: 36px;
+    font-family: "Noto Serif KR";
 `;
 
-export const H4 = styled.div`
-    font-family: "Noto Serif KR";
+export const H4 = styled.h4`
     font-size: 24px;
+    font-family: "Noto Serif KR";
 `;
