4ac1851687e81eae702433eab7ba8c60bd1b6fdf
Hyerin Sung <hyerinx@ewhain.net>
2024-03-12 17:08:58 +0900

    feat: Add Space styled-components
    - 요소 간 간격에 대하여 미리 값을 정해놓은 styled-components를 추가했다.

---
+++ src/features/ui/Space.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Space100 = styled.div`
+    margin-top: 100px;
+`;
+export const Space70 = styled.div`
+    margin-top: 70px;
+`;
+export const Space50 = styled.div`
+    margin-top: 50px;
+`;
+export const Space30 = styled.div`
+    margin-top: 30px;
+`;
+export const Space20 = styled.div`
+    margin-top: 20px;
+`;
