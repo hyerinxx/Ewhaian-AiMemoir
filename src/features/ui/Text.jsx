ec00e09110dcb7970a6f2140e2e1181d8e8ad9f3
Hyerin Sung <hyerinx@ewhain.net>
2024-03-12 16:56:38 +0900

    style: 불필요한 공백 줄 삭제

--- src/features/ui/Text.jsx
+++ src/features/ui/Text.jsx
@@ -4,17 +4,14 @@ export const H1 = styled.h1`
     font-size: 60px;
     font-family: "Noto Serif KR";
 `;
-
 export const H2 = styled.h2`
     font-size: 48px;
     font-family: "Noto Serif KR";
 `;
-
 export const H3 = styled.h3`
     font-size: 36px;
     font-family: "Noto Serif KR";
 `;
-
 export const H4 = styled.h4`
     font-size: 24px;
     font-family: "Noto Serif KR";
