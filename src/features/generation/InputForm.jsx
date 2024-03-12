1698691776875530b6311b991e395fe8d2431b2b
Hyerin Sung <hyerinx@ewhain.net>
2024-03-12 16:40:59 +0900

    design: InputForm Button position design
    - margin 20px
    - button의 위치를 오른쪽에 붙임

--- src/features/generation/InputForm.jsx
+++ src/features/generation/InputForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { H3 } from '@features/ui/Text';
 import { Input, Button } from 'antd';
+import styled from 'styled-components';
 const { TextArea } = Input;
+
+const ButtonContainer = styled.div`
+    margin: 20px;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+`;
 
 function InputForm() {
     return (
@@ -12,7 +20,9 @@ function InputForm() {
                     placeholder='오늘 일어난 일들을 간단히 적어주세요.'
                     style={{height: "200px"}}
                 />
-                <Button>GPT 회고록 생성해줘!</Button>
+                <ButtonContainer>
+                    <Button>GPT 회고록 생성해줘!</Button>
+                </ButtonContainer>
             </form>
         </div>
     );
