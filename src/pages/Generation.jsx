87923a7d9c6cc542de8ca7fc8a36c8188c668e41
Hyerin Sung <hyerinx@ewhain.net>
2024-03-12 17:50:21 +0900

    design: Edit Generation page horizontal align method from css to styled-components

--- src/pages/Generation.jsx
+++ src/pages/Generation.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from '@features/generation/GenerationNav';
 import Header from '@features/generation/Header';
 import InputForm from '@features/generation/InputForm';
-import './Generation.css';
 
 function Generation() {
     return (
         <main>
             <Nav></Nav>
-            <Header image="\icon\logo-aimemoir.png">심리상담사 GPT AI 회고록</Header>
-            <div id='main-container'>
-                <InputForm></InputForm>
-            </div>
+            <AppContainer>
+                <Header image="\icon\logo-aimemoir.png">심리상담사 GPT AI 회고록</Header>
+                <div id='main-container'>
+                    <InputForm></InputForm>
+                </div>
+            </AppContainer>
         </main>
     );
 }
 
 export default Generation;
+
+const AppContainer = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    max-width: 850px;
+    width: 100%;
+    margin: 0 auto;
+`;
